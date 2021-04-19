d3b55eda8d7a200c48b4f0c32708387105908269
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-04-19 17:37:30 -0300

    Estrutura modulos para txt puro

---
+++ Capture_TXT/DataMiner/PureTxt/TextInterpreter.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+class TextInterpreter:
+    def create_df(self, text:str) -> pd.DataFrame():
+        """Extract the information into text and convert it into a data frame"""
+        pass
+
---
+++ Capture_TXT/DataMiner/PureTxt/__init__.py
@@ -0,0 +1,1 @@
+from .Pendence import Pendence
