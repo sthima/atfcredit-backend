70e886d1b41531cbbc901df281e38acd20ce7fd2
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-04-21 22:14:21 -0300

    Ajusta modulo central

--- Capture_TXT/DataMiner/TableMiner.py
+++ Capture_TXT/DataMiner/TableMiner.py
@@ -2,16 +2,12 @@ import pandas as pd
 import numpy as np
 import re
 
-from .TableFinder import TableFinder
-
 from .PureTxt import *
-# from .PureTxt import Pendence, Relationship, RegistryConsults, RegistryLastFiveConsults, PaymentsHistory
 
 class TableMiner():
 
     def __init__(self, text):
         self.text = text
-        self.tf = TableFinder(text)
         self.erro_tables = set()
 
 # ----------------------- Methods to captures default tables ----------------------- #
@@ -99,97 +95,59 @@ class TableMiner():
         except:
             self.print_error(name_type)
             return 
-        
-    def get_OVERDUE_DEBT(self):
-        try:
-            table_columns = ['DATA','MODALIDADE','VALOR','TITULO','INST_COBRADORA','LOCAL']
-            name_type = '\nDIVIDA VENCIDA'
-
-            return self.tf.search_debt_table(name_type, \
-                            table_columns = table_columns, \
-                            split_line = 1, \
-                            split_space = ' ')
-        except:
-            self.erro_tables.add('DIVIDA VENCIDA') 
-            print('X -', 'DIVIDA VENCIDA')
-            return 
 
     def get_PAYMENTS_HISTORY_ASSIGNOR(self):
+        name_type = 'HISTORICO DE PAGAMENTOS - VISAO CEDENTE'
         try:
-            name_type = 'HISTORICO DE PAGAMENTOS - VISAO CEDENTE'
-            typ, split_column =  self.tf.type_txt_detect(name_type)
-            table_columns= ['MES/ANO','PONTUAL_QTD','PONTUAL_%',
-                            '8-15_QTD','8-15_%',
-                            '16-30_QTD','16-30_%',
-                            '31-60_QTD','31-60_%',
-                            '+60_QTD','+60_%',
-                            'A_VISTA_QTD','A_VISTA_QTD']
-            if not(split_column):
-                return self.tf.search_default_table('HISTORICO DE PAGAMENTOS - VISAO CEDENTE', \
-                                table_columns = table_columns, \
-                                split_line = 1, \
-                                split_space = ' ')
-            
-            else:
-                return self.tf.search_payments_table('HISTORICO DE PAGAMENTOS - VISAO CEDENTE', \
-                                    table_columns = table_columns, \
-                                    split_space = ' ')
-
-        except Exception as error:
-            self.erro_tables.add('HISTORICO DE PAGAMENTOS - VISAO CEDENTE (VALORES EM R$ MILHARES)') 
-            print('X - HISTORICO DE PAGAMENTOS - VISAO CEDENTE (VALORES EM R$ MILHARES) - ', error)
-            return 
+            return PaymentsHistoryAssignor().create_df(self.text, name_type)
+        except:
+            self.print_error(name_type)
+            return          
+        
+    def get_OVERDUE_DEBT(self):
+        name_type = '\nDIVIDA VENCIDA'
+        try:
+            return OverdueDebt().create_df(self.text, name_type)
+        except:
+            self.print_error(name_type)
+            return    
 
     def get_COMMITMENTS_EVOLUTION(self):
+        name_type = 'EVOLUCAO DE COMPROMISSOS NO MERCADO (VALORES EM R$)'
         try:
-            return self.tf.search_default_table('EVOLUCAO DE COMPROMISSOS NO MERCADO (VALORES EM R$)')
+            return Commitments().create_df(self.text, name_type)
         except:
-            self.erro_tables.add('EVOLUCAO DE COMPROMISSOS NO MERCADO (VALORES EM R$)') 
-            print('X - EVOLUCAO DE COMPROMISSOS NO MERCADO (VALORES EM R$)')
-            return 
+            self.print_error(name_type)
+            return    
 
     def get_COMMITMENTS_EVOLUTION_FACTORINGS(self):
+        name_type = 'EVOLUCAO DE COMPROMISSOS - FACTORINGS (VALORES EM R$)'
         try:
-            return self.tf.search_default_table('EVOLUCAO DE COMPROMISSOS - FACTORINGS (VALORES EM R$)', \
-                                split_line = 1, \
-                                table_columns = ['MES/ANO', 'A VENCER'])
+            return CommitmentsFactorings().create_df(self.text, name_type)
         except:
-            self.erro_tables.add('EVOLUCAO DE COMPROMISSOS - FACTORINGS (VALORES EM R$)') 
-            print('X - EVOLUCAO DE COMPROMISSOS - FACTORINGS (VALORES EM R$)')
-            return 
+            self.print_error(name_type)
+            return    
 
     def get_COMMITMENTS_EVOLUTION_ASSIGNOR(self):
+        name_type = 'EVOLUCAO DE COMPROMISSOS - VISAO CEDENTE (VALORES EM R$)'
         try:
-            name_type = 'EVOLUCAO DE COMPROMISSOS - VISAO CEDENTE (VALORES EM R$)'
-            typ, split_column =  self.tf.type_txt_detect(name_type)
-            if typ:
-                return self.tf.search_evolution_table(name_type, \
-                                                    split_line = 1, \
-                                                    split_column = split_column, \
-                                                    split_space = '([..\|])')
-            else:
-                return self.tf.search_default_table(name_type, split_column = split_column, split_line = split_column)
-        except Exception as error:
-            self.erro_tables.add(name_type) 
-            print('X - '+name_type, ' - ', error)
-            return 
+            return CommitmentsAssignor().create_df(self.text, name_type)
+        except:
+            self.print_error(name_type)
+            return    
 
     def get_MARKET_BUSINESS_REFERENCES(self):
+        name_type = 'REFERENCIAIS DE NEGOCIOS NO MERCADO (VALORES EM R$)'
         try:
-            return self.tf.search_default_table('REFERENCIAIS DE NEGOCIOS NO MERCADO (VALORES EM R$)', \
-                                split_space = '    ')
+            return BuisinessReferencesMarket().create_df(self.text, name_type)
         except:
-            self.erro_tables.add('REFERENCIAIS DE NEGOCIOS NO MERCADO (VALORES EM R$)') 
-            print('X - REFERENCIAIS DE NEGOCIOS NO MERCADO (VALORES EM R$)')
-            return 
+            self.print_error(name_type)
+            return    
 
     def get_TERM_BUSINESS_REFERENCES(self):
+        name_type = 'REFERENCIAIS DE NEGOCIOS A PRAZO - FACTORINGS (VALORES EM R$)'
         try:
-            return self.tf.search_default_table('REFERENCIAIS DE NEGOCIOS A PRAZO - FACTORINGS (VALORES EM R$)',  \
-                                split_space = '    ',  \
-                                split_line = 1,  \
-                                table_columns = ['DATA', 'VALOR', 'MEDIA'])
+            return BuisinessReferencesTerm().create_df(self.text, name_type)
         except:
-            self.erro_tables.add('REFERENCIAIS DE NEGOCIOS A PRAZO - FACTORINGS (VALORES EM R$)') 
-            print('X - REFERENCIAIS DE NEGOCIOS A PRAZO - FACTORINGS (VALORES EM R$)')
-            return 
+            self.print_error(name_type)
+            return    
