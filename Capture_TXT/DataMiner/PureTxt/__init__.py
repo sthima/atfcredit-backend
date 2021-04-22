159c5bc00c6007465026323b0dcf99b9db9df0c5
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-04-21 22:14:32 -0300

    Importa todos os modulos

--- Capture_TXT/DataMiner/PureTxt/__init__.py
+++ Capture_TXT/DataMiner/PureTxt/__init__.py
@@ -1,4 +1,8 @@
-from .Pendence import Pendence
-from .Relationship import Relationship
-from .RegistryConsults import RegistryConsults, RegistryLastFiveConsults
-from .PaymentsHistory import PaymentsHistory,PaymentsHistoryMarket
+from .BuisinessReferences import *
+from .RegistryConsults import *
+from .PaymentsHistory import *
+from .Relationship import *
+from .OverdueDebt import *
+from .Commitments import *
+from .Pendence import *
+
