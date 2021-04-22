c32d391e9ba81a0db7ab761f016adfe98edb8145
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-04-22 11:15:43 -0300

    Ajuste de indentacao

--- Capture_TXT/DataMiner/PureTxt/BuisinessReferences.py
+++ Capture_TXT/DataMiner/PureTxt/BuisinessReferences.py
@@ -14,11 +14,7 @@ class BuisinessReferencesMarket(TextInterpreter):
         vector_aux = np.array(vector_aux)
         table_vector = vector_aux[:np.where(vector_aux == '')[0][0]]
 
-        line_aux, table_columns, table_name = self._default_search(text= text,\
-                                                                   table_columns= table_columns,\
-                                                                   table_vector = table_vector,\
-                                                                   split_line = split_line,\    
-                                                                   split_space = split_space)
+        line_aux, table_columns, table_name = self._default_search(text= text,table_columns = table_columns,table_vector = table_vector,split_line = split_line,split_space = split_space)
 
         df = self._build_df(line_aux, table_columns).reset_index(drop = True)
         name_table = re.sub(' +', ' ', name_type)
