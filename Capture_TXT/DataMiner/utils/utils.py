b7851141e8a2dda7fe680f5f58b9e625ba2b37f7
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-04-28 15:33:31 -0300

    Modulo de Utils para impoirtaçao do fundo_serasa

---
+++ Capture_TXT/DataMiner/utils/utils.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import numpy as np
+import os
+
+
+def import_serasa_list():
+    fundos_serasa = []
+    file_name = 'Lista de fundos na Serasa.txt'
+    with open(file_name) as infile:
+        for line in infile: 
+            aux = line.replace('\n','').strip().upper()
+            if len(aux) > 0:
+                fundos_serasa.append(unidecode(aux))
+                
+    return pd.Series(fundos_serasa)
+
+
+
+fundos_serasa = import_serasa_list()
