c190bc89fb44687bf97dab72b517438133e2a7da
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-05-13 09:07:31 -0300

    Generaliza uso das colecoes do mongo

--- Capture_TXT/DataMiner/DataBaseManager.py
+++ Capture_TXT/DataMiner/DataBaseManager.py
@@ -10,24 +10,24 @@ class DataBaseManager():
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
         return myclient
 
-    def __get_mycol(self):
+    def __get_mycol(self, collection):
         myclient = self.__get_connection()
         mydb = myclient["ATFCredit"]
-        mycol = mydb["PureTXT"]
+        mycol = mydb[collection]
 
         return mycol
         
 
-    def save_PURE_TXT(self, mylist):
-        x = self. __get_mycol().insert_many(mylist)
+    def save_TXT(self, mylist, collection = "Txt_tables"):
+        x = self. __get_mycol(collection).insert_many(mylist)
         # print(x.inserted_ids)
 
-    def get_all_base(self):
-        tables = self. __get_mycol().find({})
+    def get_all_base(self, collection = "Txt_tables"):
+        tables = self. __get_mycol(collection).find({})
         return tables
     
-    def get_tables_of_CNPJ(self, cnpj):
-        tables = self. __get_mycol().find_one({"cnpj": str(cnpj)})
+    def get_tables_of_CNPJ(self, cnpj, collection = "Txt_tables"):
+        tables = self. __get_mycol(collection).find_one({"cnpj": str(cnpj)})
         tables.pop('_id',None)
         tables.pop('cnpj',None)
 
