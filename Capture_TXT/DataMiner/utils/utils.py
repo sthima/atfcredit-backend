a173864f913496c25c960c8a5e93c35e6f84b091
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-05-17 11:30:36 -0300

    Cria func para tratamento dos numeros com strings

--- Capture_TXT/DataMiner/utils/utils.py
+++ Capture_TXT/DataMiner/utils/utils.py
@@ -3,7 +3,6 @@ from unidecode import unidecode
 import pandas as pd
 import numpy as np
 import json
-import os
 
 
 def import_serasa_list():
@@ -27,6 +26,41 @@ class CustomEncoder(json.JSONEncoder):
             return obj.tolist()
         else:
             return super(CustomEncoder, self).default(obj)
+
+
+class ClearText():
+    @staticmethod
+    def convert_text_to_float(line):
+        line_aux = []
+
+        for x in line:
+            try:
+                if not(pd.isna(x)):
+                    x = x.replace('%', '')
+                    mult = 1
+                    if x.find('MIL') >= 0:
+                        mult = 1000
+                    else: 
+                        mult = 1000000
+                    
+                    numbers = x.split('A')
+
+                    numbers = [i.replace(',','.') for i in numbers]
+                    numbers = [i.replace('MIL','') for i in numbers]
+                    numbers = [i.replace('MI','') for i in numbers]
+                    numbers = [i.replace('M','') for i in numbers]
+                    numbers = [i.strip() for i in numbers]
+                    numbers = np.array(numbers)
+                    numbers = numbers[numbers!='']
+                    numbers = [float(i)*mult for i in numbers]
+
+                    line_aux.append(numbers[0])
+                else:
+                    line_aux.append(0)
+            except:
+                line_aux.append(np.nan)
+
+        return pd.Series(line_aux)
 
 
 fundos_serasa = import_serasa_list()
