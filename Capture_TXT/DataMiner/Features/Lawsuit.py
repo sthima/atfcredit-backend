515a73a78d739ca8289f7573d742a7b58b6a4ba9
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-05-18 15:45:56 -0300

    Tratamento de erros da geracao e features

--- Capture_TXT/DataMiner/Features/Lawsuit.py
+++ Capture_TXT/DataMiner/Features/Lawsuit.py
@@ -6,10 +6,12 @@ class Lawsuit():
     def create_feature(self, df):
         try:
             aux_df = pd.DataFrame(df['ACAO JUDICIAL'])
+            aux_df['DATA'] = pd.to_datetime(aux_df['DATA'], errors = 'coerce', format = '%d/%m/%Y')
+            
         except:
             return {'ACAO JUDICIAL': np.nan}
             
-        aux_df['DATA'] = pd.to_datetime(aux_df['DATA'], errors = 'coerce', format = '%d/%m/%Y')
+        
 
         return {'9_NATUREZA_MAIS_PRESENTE':self.most_present_lawsuit(aux_df),
             '9_TOTAL_ACAO_JUDICIAL':self.total_lawsuit(aux_df),
