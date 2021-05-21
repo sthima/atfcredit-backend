c0771c86bceb12e80a4f99b35cd5569f654e83ba
RafaelCosta <rafael.costa@saude.ce.gov.br>
2021-05-21 09:55:07 -0300

    Metodo de captura e update da pase de treino e predicao

--- Capture_TXT/DataMiner/DataBaseManager.py
+++ Capture_TXT/DataMiner/DataBaseManager.py
@@ -6,32 +6,36 @@ class DataBaseManager():
     def __init__(self):
         pass
 
-    def __get_connection(self):
-        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-        return myclient
-
     def __get_mycol(self, collection):
-        myclient = self.__get_connection()
+        myclient = pymongo.MongoClient("mongodb://localhost:27017/")
         mydb = myclient["ATFCredit"]
         mycol = mydb[collection]
 
-        return mycol
-        
+        return mycol        
 
     def save_TXT(self, mylist, collection = "Txt_tables"):
-        x = self. __get_mycol(collection).insert_many(mylist)
-        # print(x.inserted_ids)
+        self. __get_mycol(collection).insert_many(mylist)
 
     def get_all_base(self, collection = "Txt_tables"):
         tables = self. __get_mycol(collection).find({})
         return tables
+
+    def get_base_to_train(self, collection = "Txt_features"):
+        cursor = self.__get_mycol(collection).find({'result': {'$ne': -1}})
+        return pd.DataFrame(list(cursor))
     
-    def get_tables_of_CNPJ(self, cnpj, collection = "Txt_tables"):
-        tables = self. __get_mycol(collection).find_one({"cnpj": str(cnpj)})
-        tables.pop('_id',None)
-        tables.pop('cnpj',None)
+    def get_base_to_predict(self, collection = "Txt_features"):
+        cursor = self.__get_mycol(collection).find({'result': -1})
+        return pd.DataFrame(list(cursor))
+    
+    def get_one(self, cnpj, txt_name, collection = "Txt_features"):
+        cursor = self.__get_mycol(collection).find({'cnpj': cnpj, 'txt_file': txt_name})
+        return pd.DataFrame(list(cursor)).iloc[:1]
 
-        for k in tables.keys():
-            tables[k] = pd.DataFrame(tables[k]) 
+    def set_prediction(self, df, collection = 'Txt_features'):
+        for _,j in df.iterrows():
+            myquery = { "txt_file": j['txt_file'] }
+            newvalues = { "$set": { "prediction": j['prediction'] } }
 
-        return tables
+            self.__get_mycol(collection).update_one(myquery, newvalues)
+    
